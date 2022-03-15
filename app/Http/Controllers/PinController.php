867e92d40d4e1710ba88421e88f6dc7dad5da4b8
AsmaaDev <a.muhammed3601@gmail.com>
2022-03-15 19:36:04 +0200

    complete task

---
+++ app/Http/Controllers/PinController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pin;
+use Illuminate\Http\Request;
+
+class PinController extends Controller
+{
+    
+    public function list()
+    {
+        $pins = Pin::orderBy('created_at', 'desc')->get();
+        
+        return view('home', compact('pins'));
+    }
+
+    public function create()
+    {
+        $input = ['pin' => $this->generateUniqueCode()];
+        $code = Pin::create($input);
+
+        return $code ?
+        redirect()->route('home')->with('success', 'New pin created successfully.') :
+        redirect()->route('home')->with('warning', 'Error');
+
+    }
+
+    public function generateUniqueCode()
+    {
+
+        $code = random_int(1000, 9998);
+        $arr = strval( $code ) ;
+        $length = strlen($arr);
+        for ($i = 0; $i < $length-1; $i++) {
+            if ($arr[$i] === $arr[$i + 1] ){
+                break;
+            }elseif($code = Pin::where("pin", "=", $code)->first()){
+                break;
+            }else {
+                $code = random_int(1000, 9998);
+
+            }
+        }
+        return $code;
+        
+       
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+       
+        return redirect()->route('')->withStatus(__('Category successfully created.'));
+    }
+}
